1722dc2cf040a305e9756d8969510a533bd59d9c
chick_0 <chick_0@ch1ck.xyz>
2021-09-07 23:16:07 +0900

    프로젝트 수정 & 삭제

--- app/views/manage.py
+++ app/views/manage.py
@@ -1,5 +1,6 @@
 
 from flask import Blueprint
+from flask import abort
 from flask import request
 from flask import redirect
 from flask import url_for
@@ -52,20 +53,71 @@ def write_post():
     db.session.add(project)
     db.session.commit()
 
-    return redirect(url_for("member.read.project", project_id=project.id))
+    return redirect(url_for("member.read.project", name=user.name, project_id=project.id))
 
 
-@bp.get("/edit")
-def edit():
+@bp.get("/edit/<int:project_id>")
+def edit(project_id: int):
     if not check_login():
         return redirect(url_for("member.session.login"))
 
-    return "edit.write"
+    user = get_user_from_session()
+
+    project = Project.query.filter_by(
+        id=project_id,
+        owner=user.id
+    ).first()
+
+    if project is None:
+        return abort(404)
+
+    return render_template(
+        "manage/edit.html",
+        title=project.title,
+        html=project.html
+    )
 
 
-@bp.post("/edit")
-def edit_post():
+@bp.post("/edit/<int:project_id>")
+def edit_post(project_id: int):
     if not check_login():
         return redirect(url_for("member.session.login"))
 
-    return "manage.edit_post"
+    user = get_user_from_session()
+
+    project = Project.query.filter_by(
+        id=project_id,
+        owner=user.id
+    ).first()
+
+    if project is None:
+        return abort(404)
+
+    project.title = request.form.get("title", project.title)
+    project.html = request.form.get("editor", project.html)
+    project.public = True if request.form.get("public", "false") == "true" else False
+
+    db.session.commit()
+
+    return redirect(url_for("member.read.project", name=user.name, project_id=project_id))
+
+
+@bp.get("/delete/<int:project_id>")
+def delete(project_id: int):
+    if not check_login():
+        return redirect(url_for("member.session.login"))
+
+    user = get_user_from_session()
+
+    project = Project.query.filter_by(
+        id=project_id,
+        owner=user.id
+    ).first()
+
+    if project is None:
+        return abort(403)
+
+    db.session.delete(project)
+    db.session.commit()
+
+    return redirect(url_for("project.show_all"))
