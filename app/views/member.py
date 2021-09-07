6bb82040837bdf783f55db55648e7d05e630ed9d
chick_0 <chick_0@ch1ck.xyz>
2021-09-07 23:25:06 +0900

    멤버 보여주기

--- app/views/member.py
+++ app/views/member.py
@@ -1,6 +1,11 @@
 
 from flask import Blueprint
+from flask import abort
+from flask import request
+from flask import render_template
 
+from app.models import Member
+from app.models import Project
 from .read import bp as read_bp
 from .session import bp as session_bp
 
@@ -14,6 +19,31 @@ bp.register_blueprint(read_bp)
 bp.register_blueprint(session_bp)
 
 
-@bp.get("/<string:user_name>")
-def show(user_name: str):
-    return f"member.show : {user_name}"
+@bp.get("/<string:name>")
+def show(name: str):
+    member = Member.query.filter_by(
+        name=name
+    ).first()
+
+    if member is None:
+        return abort(404)
+
+    try:
+        page = int(request.args.get("page", "1"))
+
+        if page <= 0:
+            page = 1
+    except ValueError:
+        page = 1
+
+    projects = Project.query.filter_by(
+        owner=member.id
+    ).order_by(
+        Project.id.desc()
+    ).paginate(page)
+
+    return render_template(
+        "member/show.html",
+        member=member,
+        projects=projects
+    )
