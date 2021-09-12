19ae4d5accf6a370eb73baeeb496e5397eff4f2e
chick_0 <chick_0@ch1ck.xyz>
2021-09-13 02:57:48 +0900

    링크 버튼 추가/수정/삭제 + 오류 메시지

--- app/views/link.py
+++ app/views/link.py
@@ -7,7 +7,6 @@ from flask import url_for
 from flask import render_template
 
 from app import db
-from app.models import Member
 from app.models import Project
 from app.models import Link
 from app.utils import check_login
@@ -50,14 +49,80 @@ def view(project_id: int):
 
 @bp.get("/<int:project_id>/delete/<int:link_id>")
 def delete(project_id: int, link_id: int):
-    return "manage.link.delete"
+    if not check_login():
+        return redirect(url_for("manage.session.login"))
+
+    user = get_user_from_session()
+
+    project = Project.query.filter_by(
+        id=project_id,
+        owner=user.id
+    ).first()
+
+    if project is not None:
+        Link.query.filter_by(
+            id=link_id,
+            project_id=project.id
+        ).delete()
+
+        db.session.commit()
+
+    return redirect(url_for("manage.link.view", project_id=project_id))
 
 
 @bp.post("/<int:project_id>/edit/<int:link_id>")
 def edit_post(project_id: int, link_id: int):
-    return "manage.link.edit_post"
+    if not check_login():
+        return redirect(url_for("manage.session.login"))
+
+    user = get_user_from_session()
+
+    project = Project.query.filter_by(
+        id=project_id,
+        owner=user.id
+    ).first()
+
+    if project is not None:
+        link = Link.query.filter_by(
+            id=link_id,
+            project_id=project.id
+        ).first()
+
+        if link is not None:
+            link.color = request.form.get("color", link.color)[:60]
+            link.text = request.form.get("text", link.text)[:100]
+            link.url = request.form.get("url", link.url)[:256]
+
+        db.session.commit()
+
+    return redirect(url_for("manage.link.view", project_id=project_id))
 
 
 @bp.post("/<int:project_id>/create")
 def create_post(project_id: int):
-    return "manage.link.create_post"
+    if not check_login():
+        return redirect(url_for("manage.session.login"))
+
+    user = get_user_from_session()
+
+    project = Project.query.filter_by(
+        id=project_id,
+        owner=user.id
+    ).first()
+
+    if project is not None:
+        if Link.query.filter_by(
+            project_id=project_id
+        ).count() >= 10:
+            return redirect(url_for("manage.link.view", project_id=project_id, e="tml"))
+
+        link = Link()
+        link.project_id = project_id
+        link.color = request.form.get("color", "btn-primary")[:60]
+        link.text = request.form.get("text", "")[:100]
+        link.url = request.form.get("url", "")[:256]
+
+        db.session.add(link)
+        db.session.commit()
+
+    return redirect(url_for("manage.link.view", project_id=project_id))
