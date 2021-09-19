8d52d1183278fc0c7f5ed46e785756109bcf12a7
chick_0 <chick_0@ch1ck.xyz>
2021-09-20 03:15:34 +0900

    버튼 스크립트 변경

--- app/static/button.js
+++ app/static/button.js
@@ -6,21 +6,17 @@ function btn_init(project_id){
             id: project_id
         }
     }).then(function(response){
-        console.log(response);
         response.data.forEach(function(link){
-            let color = link.color;
-            let text = link.text;
-            let url = link.url;
-
-            console.log(color, text, url);
-
+            let color   = link.color;
+            let text    = link.text;
+            let url     = link.url;
             let display = document.getElementById("link-button");
+            let button  = document.createElement("a");
 
-            let button = document.createElement("a");
-            button.setAttribute("class", `btn ${color}`);
+            button.setAttribute("class", "btn " + color);
             button.setAttribute("href", url);
             button.setAttribute("target", "_blank");
-            button.setAttribute("rel", "noreferrer");
+            button.setAttribute("rel", "no" + "referrer");
             button.innerText = text;
 
             display.appendChild(button);
